d0b51fef60cd71ded138e96e61d8af295882f531
ShenSudara <sudarakainfo2000@gmail.com>
2026-01-22 18:52:34 +0530

    Initial Commit

---
+++ MovieTime/MovieTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieTimeApp.swift
+//  MovieTime
+//
+//  Created by Ashen Sudaraka on 2026-01-22.
+//
+
+import SwiftUI
+
+@main
+struct MovieTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
